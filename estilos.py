64d5a1b61d6b26d75e164add4df37b79c4a24393
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2022-12-05 00:33:42 -0300

    estilos utilizados na criação do jogo

---
+++ estilos.py
@@ -0,0 +1,12 @@
+# Estilos para texto utilizados no projeto
+estilos = {
+  'NEGRITO': "\u001b[1m",
+  'MAGENTA_NEGRITO': "\u001b[35;1m",
+  'VERMELHO': "\u001b[31m",
+  'VERDE': "\u001b[32m",
+  'RESET': "\u001b[0m",
+  'VERMELHO_BRILHANTE': "\u001b[31;1m",
+  'BLACK': "\u001b[30m",
+  'BACKGROUND_WHITE': "\u001b[47m",
+  'REVERSED': "\u001b[7m"
+}
