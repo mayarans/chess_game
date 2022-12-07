bf0576b1bf422fbd3188d040b11901920531dad9
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2022-12-07 15:45:01 -0300

    adicionando estilo às mensagens de erro

--- funcs_auxiliares.py
+++ funcs_auxiliares.py
@@ -1,4 +1,5 @@
 import time, sys
+import estilos as e
 
 # Letras que representam as colunas da matriz tabuleiro
 letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -24,19 +25,19 @@ def verificarOrigemValida(tabuleiro, origem, cor):
       if tabuleiro[lista[0]][lista[1]] not in pecasPretas:
         resposta = 'ok'
       else:
-        resposta = 'Você está tentando mover uma peça preta. Aguarde sua vez!'
+        resposta = f'\n{e.estilos["VERMELHO"]}Você está tentando mover uma peça preta. Aguarde sua vez!{e.estilos["RESET"]}\n'
     # Verificando se a posição de origem contém uma peça preta
     elif cor == 'preta' and tabuleiro[lista[0]][lista[1]] not in quadrados:
       if tabuleiro[lista[0]][lista[1]] not in pecasBrancas:
         resposta = 'ok'
       else:
-        resposta = 'Você está tentando mover uma peça branca. Aguarde sua vez!'
+        resposta = f'\n{e.estilos["VERMELHO"]}Você está tentando mover uma peça branca. Aguarde sua vez!{e.estilos["RESET"]}\n'
     else:
-      resposta = 'Não há peças nesta posição. Tente novamente!'
+      resposta = f'\n{e.estilos["VERMELHO"]}Não há peças nesta posição. Indique uma posição válida!{e.estilos["RESET"]}\n'
   except (ValueError, IndexError):
-    resposta = 'Esta posição não está dentro dos limites do tabuleiro. Tente novamente!'
-  return resposta
+    resposta = f'\n{e.estilos["VERMELHO"]}Esta posição não está dentro dos limites do tabuleiro. Tente novamente!{e.estilos["RESET"]}\n'
 
+  return resposta
 
 # Função auxiliar para simular o carregamento do jogo de xadrez
 def loading():
