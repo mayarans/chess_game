ca93a60802bf507d863fae705181f054748c05aa
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2022-12-17 17:10:08 -0300

    correção de formatação e retirada de função duplicada

--- xeque.py
+++ xeque.py
@@ -7,57 +7,54 @@ def xeque(tabuleiro,linha,coluna,cor):
 
     #Verificação se há um peão na diagonal imediata antes de verificar as demais casas
     if cor == 'branca':
-        if coluna<7:
-            if tabuleiro[linha - 1][coluna + 1] == f.corEpeca(f.inverteCor(cor),'peao'):
+        if coluna < 7:
+            if tabuleiro[linha - 1][coluna + 1] == f.corEpeca(f.inverteCor(cor), 'peao'):
                 return True
-        if coluna>0:
-            if tabuleiro[linha -1][coluna - 1] == f.corEpeca(f.inverteCor(cor),'peao'):
+        if coluna > 0:
+            if tabuleiro[linha -1][coluna - 1] == f.corEpeca(f.inverteCor(cor), 'peao'):
                 return True
     else:
-        if coluna<7:
-            if tabuleiro[linha + 1][coluna + 1] == f.corEpeca(f.inverteCor(cor),'peao'):
+        if coluna < 7:
+            if tabuleiro[linha + 1][coluna + 1] == f.corEpeca(f.inverteCor(cor), 'peao'):
                 return True
-        if coluna>0:
-            if tabuleiro[linha + 1][coluna - 1] == f.corEpeca(f.inverteCor(cor),'peao'):
+        if coluna > 0:
+            if tabuleiro[linha + 1][coluna - 1] == f.corEpeca(f.inverteCor(cor), 'peao'):
                 return True
     
     if f.verificacaoDiagonal(tabuleiro, linha, coluna, -1, 1, -1, 8,cor):
         return True #Casas superiores direita 
   
-    if f.verificacaoDiagonal(tabuleiro, linha, coluna, -1, -1, -1,-1,cor):
+    if f.verificacaoDiagonal(tabuleiro, linha, coluna, -1, -1, -1, -1, cor):
         return True   #Casas superiores esquerda
     
-    if f.verificacaoDiagonal(tabuleiro, linha, coluna, +1, +1, 8,8,cor):
+    if f.verificacaoDiagonal(tabuleiro, linha, coluna, +1, +1, 8, 8, cor):
         return True #Casas inferior direita 
     
-    if f.verificacaoDiagonal(tabuleiro, linha, coluna, +1, -1, 8,-1,cor):
+    if f.verificacaoDiagonal(tabuleiro, linha, coluna, +1, -1, 8, -1, cor):
         return True #Casas inferior esquerda 
     
     #VERIFICAÇÃO DAS CASAS EM "L" (Movimento do cavalo)
-    for listas in f.possibilidadesL(linha,coluna):
+    for listas in m.possibilidadesL([linha, coluna]):
         i  = listas[0]
         j = listas[1]
-        if tabuleiro[i][j]==f.corEpeca(f.inverteCor(cor),'cavalo'):
+        if tabuleiro[i][j] == f.corEpeca(f.inverteCor(cor), 'cavalo'):
             return True
 
     #VERIFICAÇÃO DAS CASAS HORIZINTAIS (Movimento da torre e da rainha)
-    if f.verificacaoHorizontalEVertical(tabuleiro,linha,coluna,-1,0,-1,8,cor):
+    if f.verificacaoHorizontalEVertical(tabuleiro, linha, coluna, -1, 0, -1, 8, cor):
         return True #cima
-    if f.verificacaoHorizontalEVertical(tabuleiro,linha,coluna,+1,0,8,8,cor):
+    if f.verificacaoHorizontalEVertical(tabuleiro, linha, coluna, 1, 0, 8, 8, cor):
         return True #baixo
-    if f.verificacaoHorizontalEVertical(tabuleiro,linha,coluna,0,+1,8,8,cor):
+    if f.verificacaoHorizontalEVertical(tabuleiro, linha, coluna, 0, 1, 8, 8, cor):
         return True #direita
-    if f.verificacaoHorizontalEVertical(tabuleiro,linha,coluna,0,-1,8,-1,cor):
+    if f.verificacaoHorizontalEVertical(tabuleiro, linha, coluna, 0, -1, 8, -1, cor):
         return True #esquerda
    
-
     return False
 
-def xequeMate(tabuleiro,cor):
+def xequeMate(tabuleiro, cor):
     posicaoRei=f.localizarRei(tabuleiro, cor)
-    if (xeque(tabuleiro,posicaoRei[0],posicaoRei[1],cor)) and (m.reiPossibilidades(tabuleiro,f.valorOrigem(posicaoRei[1],posicaoRei[0]),cor))==[]:
+    if (xeque(tabuleiro,posicaoRei[0],posicaoRei[1],cor)) and (m.reiPossibilidades(tabuleiro, f.valorOrigem(posicaoRei[1], posicaoRei[0]), cor)) == []:
         return True
     else:
         return False
-
-
