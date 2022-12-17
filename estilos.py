d49bc1aa853f86f24fd339fdcfcec34d2c7874d2
BoloSalgadoo <ttf1880@gmail.com>
2022-12-17 13:46:30 -0300

    Adição do estilo de fundo verde

--- estilos.py
+++ estilos.py
@@ -5,9 +5,10 @@ estilos = {
   'MAGENTA_NEGRITO': "\u001b[35;1m",
   'VERMELHO': "\u001b[31m",
   'VERDE': "\u001b[32m",
-  'RESET': "\u001b[0m",
+  'REDEFINIR': "\u001b[0m",
   'VERMELHO_BRILHANTE': "\u001b[31;1m",
-  'BLACK': "\u001b[30m",
-  'BACKGROUND_WHITE': "\u001b[47m",
-  'REVERSED': "\u001b[7m"
+  'PRETO': "\u001b[30m",
+  'FUNDO_BRANCO': "\u001b[47m",
+  'REVERSO': "\u001b[7m",
+  'FUNDO_VERDE' : "\033[42m"
 }
